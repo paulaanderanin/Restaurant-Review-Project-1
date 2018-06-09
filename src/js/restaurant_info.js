09d161b7b2f57148987fe9272b7afa4926409416
panderanin <ppapola76@gmail.com>
2018-06-09 16:47:02 -0400

    v2

--- src/js/restaurant_info.js
+++ src/js/restaurant_info.js
@@ -143,6 +143,7 @@ fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
+  li.setAttribute('aria-label',document.getElementById('breadcrumb'));
   breadcrumb.appendChild(li);
 }
 
